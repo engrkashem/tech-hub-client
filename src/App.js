64be1bc8295488e0414f824fdb6b4b58166d5935
engrkashem <kashemaust@gmail.com>
2022-04-29 16:07:47 +0600

    initialised with tailwind, MUI, react router, react toastify

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import About from "./Pages/About/About/About";
+import Login from "./Pages/Authentication/Login/Login";
+import Register from "./Pages/Authentication/Register/Register";
+import Blog from "./Pages/Blogs/Blog";
+import Footer from "./Pages/Footer/Footer/Footer";
+import Header from "./Pages/Header/Header";
+import Home from "./Pages/Home/Home/Home";
+import NotFound from "./Pages/NotFound/NotFound";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/home" element={<Home />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/register" element={<Register />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/blog" element={<Blog />}></Route>
+        <Route path="*" element={<NotFound />}></Route>
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
