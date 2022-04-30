7e879bfa45dd4c00f627d19e87b8c15898d3312f
engrkashem <kashemaust@gmail.com>
2022-04-30 21:57:17 +0600

    register form design added

--- src/Pages/Authentication/Login/Login.js
+++ src/Pages/Authentication/Login/Login.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import '../../../Styles/Login/Login.css';
 import google from '../../../images/logoes/icons-google.png'
+import { Button } from '@mui/material';
 
 const Login = () => {
     //vanilla CSS scripted on Login.css file
@@ -11,14 +12,15 @@ const Login = () => {
                 <form>
                     <span className="text">Login</span>
                     <div className="inputs">
-                        <input type="email" placeholder="Email" />
-                        <input type="password" placeholder="Password" />
-                        <button type="submit">LOGIN</button>
+                        <input type="email" placeholder="Email" required />
+                        <input type="password" placeholder="Password" required />
+                        <Button type="submit">LOGIN</Button>
                     </div>
                 </form>
                 <h5 className=' text-gray-500 mt-5'>New to Tech Hub? <Link className=' text-blue-600 font-medium' to={'/register'}>Register</Link></h5>
-                <h5 className=' text-gray-500 mt-2'>Forgot Password? <Link className=' text-blue-600 font-medium' to={'/register'}>Reset</Link></h5>
-                <button className='flex items-center justify-center'><img width={'40px'} src={google} alt="google logo" /> <span className=' ml-5'>Google Login</span></button>
+                <h5 className=' text-gray-500 mt-2 mb-8'>Forgot Password? <span className=' text-blue-600 font-medium cursor-pointer' >Reset</span></h5>
+                <Button ><img width={'40px'} src={google} alt="google logo" /> <span className=' ml-5 font-medium'>Google Login</span></Button>
+
             </div>
         </div>
     );
--- src/Pages/Authentication/Register/Register.js
+++ src/Pages/Authentication/Register/Register.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import google from '../../../images/logoes/icons-google.png'
 
 const Register = () => {
     return (
-        <div>
-            <h2>Register</h2>
+        <div className='login-container' >
+            <div className="container">
+                <form>
+                    <span className="text">Register</span>
+                    <div className="inputs">
+                        <input type="text" placeholder="Name" required />
+                        <input type="email" placeholder="Email" required />
+                        <input type="password" placeholder="Password" required />
+                        <button type="submit">Register</button>
+                    </div>
+                </form>
+                <h5 className=' text-gray-500 mt-5'>Already Registered? <Link className=' text-blue-600 font-medium' to={'/login'}>Login</Link></h5>
+
+                <button className='flex items-center justify-center'><img width={'40px'} src={google} alt="google logo" /> <span className=' ml-5'>Google Login</span></button>
+            </div>
         </div>
     );
 };
