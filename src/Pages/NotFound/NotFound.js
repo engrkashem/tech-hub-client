ec06936cdc045da43d2810ca673505cebc827e48
engrkashem <kashemaust@gmail.com>
2022-05-06 20:23:15 +0600

    not foun dpage added

--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import '../../Styles/NotFound/NotFound.css';
 
 const NotFound = () => {
+    const navigate = useNavigate();
     return (
-        <div>
-            <h2>Page not Found</h2>
+        <div className='not-found-container' >
+            <h2 className='not-found-text inline relative top-1/2 text-4xl text-red-500 font-bold'>Page not Found</h2>
+            <button onClick={() => navigate('/home')} className='not-found-text text-black relative top-1/2 w-44' >BACK TO HOME</button>
         </div>
     );
 };
